409254421c1c8d28d350eaccb5658d99a6ebdbf6
Johnson Fu <johnson.fu@gmail.com>
2023-04-01 19:43:20 +0800

    global favorites

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -38,7 +38,23 @@ class Products with ChangeNotifier {
     ),
   ];
 
+  var _showFavoritesOnly = false;
+
+  void showFavoritesOnly() {
+    _showFavoritesOnly = true;
+    notifyListeners();
+  }
+
+  void showAll() {
+    _showFavoritesOnly = false;
+    notifyListeners();
+  }
+
   List<Product> get items {
+    if (_showFavoritesOnly) {
+      return _items.where((element) => element.isFavorite == true).toList();
+    }
+
     return [..._items];
   }
 
--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -1,19 +1,80 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../providers/products.dart';
 import '../widgets/product_grid.dart';
 
-import '../providers/product.dart';
+enum FilterOptions { Favorites, All }
 
 class ProductsOverviewScreen extends StatelessWidget {
   ProductsOverviewScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final productsContainer = Provider.of<Products>(context, listen: false);
+
     return Scaffold(
-      appBar: AppBar(title: Text('MyShop')),
+      appBar: AppBar(title: Text('MyShop'), actions: [
+        const BottomSheetExample(),
+        PopupMenuButton(
+          onSelected: (value) {
+            if (value == FilterOptions.Favorites) {
+              productsContainer.showFavoritesOnly();
+            } else {
+              productsContainer.showAll();
+            }
+          },
+          icon: Icon(Icons.more_vert),
+          itemBuilder: (context) {
+            return [
+              PopupMenuItem(
+                  child: Text('Only Favorites'),
+                  value: FilterOptions.Favorites),
+              PopupMenuItem(child: Text('Show All'), value: FilterOptions.All),
+            ];
+          },
+        ),
+      ]),
       // grid view
       // the gird item ratio is for example
       // 300px width vs 200px height => 3 / 2
       body: ProductGrid(),
+    );
+  }
+}
+
+class BottomSheetExample extends StatelessWidget {
+  const BottomSheetExample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ElevatedButton(
+        child: const Icon(Icons.info_sharp),
+        onPressed: () {
+          showModalBottomSheet<void>(
+            context: context,
+            builder: (BuildContext context) {
+              return Container(
+                height: 500,
+                color: Colors.amber,
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      const Text('Modal BottomSheet'),
+                      ElevatedButton(
+                        child: const Text('Close BottomSheet'),
+                        onPressed: () => Navigator.pop(context),
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            },
+          );
+        },
+      ),
     );
   }
 }
