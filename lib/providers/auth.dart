33243fe7909df6aad6457c559c8981c1c2c7fbec
Johnson Fu <johnson.fu@gmail.com>
2023-04-08 23:28:35 +0800

    add auth api catch error

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -2,11 +2,14 @@ import 'dart:convert';
 
 import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
+import 'package:shop_app/models/http_execption.dart';
 
 class Auth with ChangeNotifier {
   late String _token;
   late DateTime _expiryDate;
   late String _userId;
+
+  final String webkey = 'AIzaSyDOWMeAeCvsMv51GU-N3iMLcZv3ilIqZkQ';
 
   Future<void> signup(String email, String password) async {
     // https://identitytoolkit.googleapis.com/v1/accounts:signInWithCustomToken?key=[API_KEY]
@@ -14,12 +17,47 @@ class Auth with ChangeNotifier {
     // AIzaSyDOWMeAeCvsMv51GU-N3iMLcZv3ilIqZkQ
 
     String url =
-        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDOWMeAeCvsMv51GU-N3iMLcZv3ilIqZkQ';
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${webkey}';
 
-    final response = await http.post(Uri.parse(url),
-        body: jsonEncode(
-            {'email': email, 'password': password, 'returnSecureToken': true}));
+    try {
+      final response = await http.post(Uri.parse(url),
+          body: jsonEncode({
+            'email': email,
+            'password': password,
+            'returnSecureToken': true
+          }));
 
-    print(jsonDecode(response.body));
+      print(jsonDecode(response.body));
+
+      final responseData = jsonDecode(response.body);
+      if (responseData['error'] != null) {
+        throw HttpExeception(responseData['error']['message']);
+      }
+    } catch (error) {
+      rethrow;
+    }
+  }
+
+  Future<void> login(String email, String password) async {
+    String url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${webkey}';
+
+    try {
+      final response = await http.post(Uri.parse(url),
+          body: jsonEncode({
+            'email': email,
+            'password': password,
+            'returnSecureToken': true
+          }));
+
+      print(jsonDecode(response.body));
+
+      final responseData = jsonDecode(response.body);
+      if (responseData['error'] != null) {
+        throw HttpExeception(responseData['error']['message']);
+      }
+    } catch (error) {
+      rethrow;
+    }
   }
 }
